7cee38aeadf20e97a20a0bed4098a7c8ef653fe8
Roberto Moreno <roberto.morenomar@alum.uca.es>
2022-07-17 19:29:19 +0200

    More battle progress

--- Assets/Scripts/Adventure/BattleManager.cs
+++ Assets/Scripts/Adventure/BattleManager.cs
@@ -2,22 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
+    private int currentLevel;
+
+    public static bool attacking;
+
     // Start is called before the first frame update
     void Start()
     {
         if (!GameObject.Find("Avatar")) SceneManager.LoadScene(1);
 
+        //Cargando jugador
         GameObject placeholder = GameObject.Find("AvatarPlaceholder");
         GameObject avatar = GameObject.Find("Avatar");
 
         avatar.transform.position = placeholder.transform.position;
         avatar.transform.localScale = new Vector2(
             avatar.transform.localScale.x * 2, avatar.transform.localScale.y * 2);
-
         placeholder.SetActive(false);
+
+        //Cargando enemigos
+        currentLevel = Stats.mapLevel;
+        int amount = EnemyLists.levelsFinal[currentLevel - 1].enemies.Length;
+        int cont = 1;
+
+        foreach(Transform amountValue in GameObject.Find("Enemigos").transform)
+        {
+            if(cont == amount)
+            {
+                int contEnemy = 0;
+                foreach(Transform enemy in amountValue)
+                {
+                    enemy.gameObject.GetComponent<Image>().sprite =
+                        EnemyLists.levelsFinal[currentLevel - 1].enemies[contEnemy].sprite;
+                    ++contEnemy;
+                } 
+            }
+            else
+            {
+                amountValue.gameObject.SetActive(false);
+            }
+
+            cont++;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Inventory/ShowObjectInfo.cs
+++ Assets/Scripts/Inventory/ShowObjectInfo.cs
@@ -1,31 +1,71 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ShowObjectInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public static GameObject enemyInfo;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         GameObject selectedObject = eventData.pointerCurrentRaycast.gameObject;
         GameObject selectedObjectParent = selectedObject.transform.parent.parent.gameObject;
-        Debug.Log(selectedObject.name);
 
         switch(selectedObjectParent.name)
         {
             case "EnemigosList":
                 EnemyLoader.enemyInfoScreens
-                    [int.Parse(selectedObject.name.ToCharArray()[5].ToString()) - 1].SetActive(true);
+                    [int.Parse(selectedObject.name.ToCharArray()[5].ToString())-1].SetActive(true);
                 break;
 
             case "ObjetosInventory":
                 InventoryMenu.inventoryInfoScreens
                     [int.Parse(selectedObject.name.ToCharArray()[7].ToString())].SetActive(true);
                 break;
+
+            case "Enemigos":
+
+                if (!BattleManager.attacking)
+                {
+                    Enemy thisEnemy = EnemyLists.levelsFinal[Stats.mapLevel - 1].enemies
+                        [int.Parse(selectedObject.name.ToCharArray()[5].ToString()) - 1];
+
+                    //Cambiando el contenido del cuadro de información;
+                    enemyInfo.transform.GetChild(1).gameObject.GetComponent<Text>().text =
+                        thisEnemy.enemyName.ToString();
+                    enemyInfo.transform.GetChild(2).gameObject.GetComponent<Text>().text =
+                        "Ataque: " + thisEnemy.attack.ToString();
+                    enemyInfo.transform.GetChild(3).gameObject.GetComponent<Text>().text =
+                        "Defensa: " + thisEnemy.defense.ToString();
+                    enemyInfo.transform.GetChild(4).gameObject.GetComponent<Text>().text =
+                        "Ataque: " + thisEnemy.health.ToString();
+                    enemyInfo.SetActive(true);
+                }
+
+                break;
         }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        foreach (GameObject g in EnemyLoader.enemyInfoScreens) g.SetActive(false);
-        foreach (GameObject g in InventoryMenu.inventoryInfoScreens) g.SetActive(false);
+        if(SceneManager.GetActiveScene().name == "Mapa")
+        {
+            foreach (GameObject g in EnemyLoader.enemyInfoScreens) g.SetActive(false);
+            foreach (GameObject g in InventoryMenu.inventoryInfoScreens) g.SetActive(false);
+        }
+        else if (SceneManager.GetActiveScene().name == "Battle")
+        {
+            enemyInfo.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        if (GameObject.Find("EnemyInfo"))
+        {
+            enemyInfo = GameObject.Find("EnemyInfo");
+            enemyInfo.SetActive(false);
+        }
     }
 }

ce3bde7b783991e0b25d45aad490678a0d8bd43e
Roberto Moreno <roberto.morenomar@alum.uca.es>
2022-08-26 12:15:39 +0200

    bugfix 1

--- unity/loadCharacterData.php
+++ unity/loadCharacterData.php
@@ -4,11 +4,25 @@
     require_once('../config.php');
     global $USER;
 
+    //Obteniendo datos del fichero database.txt
+    $handle = fopen("database.txt", "r");
+    if ($handle) {
+        $i = 0;
+        while (($line = fgets($handle)) !== false) {
+            $linesplit = explode('=', $line);
+            switch($i) {
+                case 0: $hostname = substr($linesplit[1], 0, -2); break;
+                case 1: $username = substr($linesplit[1], 0, -2); break;
+                case 2: $password = substr($linesplit[1], 0, -2); break;
+                case 3: $database = substr($linesplit[1], 0, -2); break;
+            }
+            $i++;
+        }
+
+        fclose($handle);
+    }
+
     //Estableciendo conexión con la base de datos
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'moodle';
     $dbConnection = mysqli_connect($hostname, $username, $password, $database) or die ("Conexión con la base de datos fallida");
 
     //En primer lugar, se comprueba si el usuario ya tiene un personaje en la base de datos
--- unity/uploadInventory.php
+++ unity/uploadInventory.php
@@ -4,14 +4,28 @@
     require_once('../config.php');
     global $USER;
 
+    //Obteniendo datos del fichero database.txt
+    $handle = fopen("database.txt", "r");
+    if ($handle) {
+        $i = 0;
+        while (($line = fgets($handle)) !== false) {
+            $linesplit = explode('=', $line);
+            switch($i) {
+                case 0: $hostname = substr($linesplit[1], 0, -2); break;
+                case 1: $username = substr($linesplit[1], 0, -2); break;
+                case 2: $password = substr($linesplit[1], 0, -2); break;
+                case 3: $database = substr($linesplit[1], 0, -2); break;
+            }
+            $i++;
+        }
+
+        fclose($handle);
+    }
+
     //Estableciendo conexión con la base de datos
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'moodle';
     $dbConnection = mysqli_connect($hostname, $username, $password, $database) or die ("Conexión con la base de datos fallida");
 
-    //En primer lugar, se crea la tabla game_characters si esta no existe
+    //En primer lugar, se crea la tabla game_inventory si esta no existe
     $query = "CREATE TABLE IF NOT EXISTS game_inventory (
         ID int(11) AUTO_INCREMENT,
         USERID bigint(10),
